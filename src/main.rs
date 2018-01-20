d80a695580dca9b97880dd3f50e39d34ca3f6891
Costa Huang <costa.huang@outlook.com>
2018-01-20 18:16:10 -0500

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    for i in 1..100 {
+        if i % 5 == 0 &&  i % 6 == 0{
+            println!("fizzbuzz");
+        }
+        else if i % 5 == 0 {
+            println!("fizz");
+        }
+        else if i % 6 == 0 {
+            println!("buzz");
+        }
+        else {
+            println!("{}", i);
+        }
+    }
 }
